1cc1613b388517edd59b7b579753ca1ae2978704
Frederico J. R. Pelogia <fredpelogia@outlook.com>
2021-07-06 15:42:57 -0300

    Add 3 more registers: sp, fp, ap
    So, counting the PC register,
    we now have 36 registers:
    1 PC
    32 general purpose registers
    1 sp - stack pointer
    1 fp - frame pointer
    1 ap - argument pointer

--- banco_de_registradores.v
+++ banco_de_registradores.v
@@ -5,7 +5,11 @@ module banco_de_registradores(clk,rl1, rl2, resc, dado, h_esc, d1, d2);
 	input h_esc;
 	output [31:0] d1,d2;
 	
-	reg [31:0] x[30:0]; // 31 reg's de 32 bits
+	reg [31:0] x[34:0]; // 35 reg's de 32 bits
+        // 32 de propósito geral (GPRs)
+        // 1 sp - stack pointer
+        // 1 fp - frame pointer
+        // 1 ap - arguments pointer
 
 
 	always @(posedge(clk)) begin
