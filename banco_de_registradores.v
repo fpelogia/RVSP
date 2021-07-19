91eaae637261b42d890b16f7987e9ae4f8b07bf0
Frederico J. R. Pelogia <fredpelogia@outlook.com>
2021-07-19 11:14:11 -0300

    Reduce the number of GPRs, to match instr. fmts
    When I increased the number of registers, to fit the specific purpose
    registers, I forgot that the 5 bit field of the instructions could map
    only to 32 registers.
    Therefore, now i have reduced the number of GPRs to 29, in order to have
    32 registers in total.

--- banco_de_registradores.v
+++ banco_de_registradores.v
@@ -5,8 +5,8 @@ module banco_de_registradores(clk,rl1, rl2, resc, dado, h_esc, d1, d2);
 	input h_esc;
 	output [31:0] d1,d2;
 	
-	reg [31:0] x[34:0]; // 35 reg's de 32 bits
-        // 32 de propósito geral (GPRs)
+	reg [31:0] x[28:0]; // 35 reg's de 32 bits
+        // 29 de propósito geral (GPRs)
         // 1 sp - stack pointer
         // 1 fp - frame pointer
         // 1 ap - arguments pointer
